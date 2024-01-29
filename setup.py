94a3cb3476027b8bf4580dc3fdd9f93019e99cfa
noahyoungs <74278430+noahyoungs@users.noreply.github.com>
2024-01-29 12:29:25 +0900

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tailwind-palette",
+    version="1.0.0",
+    description="A Python package for working with the Tailwind CSS color palette.",
+    author="Noah Youngs",
+    packages=find_packages(),
+)
